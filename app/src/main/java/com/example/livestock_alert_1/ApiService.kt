8c46d4903a0a0923666919ba4c4d9dc7f42bcb63
ShibuShivansh40 <seth.shivansh67@gmail.com>
2023-06-07 14:32:02 +0530

    Added the URL Entering Feature to the Application.
    Currently, it is working fine and providing all the details and is also fetching the image.

--- app/src/main/java/com/example/livestock_alert_1/ApiService.kt
+++ app/src/main/java/com/example/livestock_alert_1/ApiService.kt
@@ -1,8 +1,9 @@
 package com.example.livestock_alert_1
 
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface ApiService {
-    @GET("http://4945-43-230-198-50.ngrok-free.app/all/")
-    suspend fun getItems(): List<Item>
+    @GET()
+    suspend fun getItems(@Url URL_Captured: String): List<Item>
 }
